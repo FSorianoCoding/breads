eeef1843c627515cee2aabfe7c89456078d4b87c
Fabian Soriano <fabo.soriano@gmail.com>
2022-09-25 13:24:06 -0700

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -5,11 +5,11 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('Index',
-      {
-        breads: Bread
-      }
-    )
+  res.render('Index',
+    {
+      breads: Bread
+    }
+  )
   // res.send(Bread)
 })
 
@@ -18,12 +18,20 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex],
+      bread: Bread[req.params.arrayIndex],
       index: req.params.arrayIndex,
     })
   } else {
@@ -37,7 +45,7 @@ breads.post('/', (req, res) => {
   if (!req.body.image) {
     req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
   }
-  if(req.body.hasGluten === 'on') {
+  if (req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
@@ -50,6 +58,17 @@ breads.post('/', (req, res) => {
 breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
+})
+
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  if (req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 module.exports = breads
